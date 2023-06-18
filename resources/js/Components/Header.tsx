8d521c114c0aa5de2b37966c2d7a78aeef9324dd
domnantas <hello@domnantas.lt>
2023-06-18 19:01:40 +0300

    Add mapbox map

--- resources/js/Components/Header.tsx
+++ resources/js/Components/Header.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 import ApplicationLogo from "./ApplicationLogo";
 
 export const Header = ({ children }: PropsWithChildren) => (
-	<header className="sticky top-0 z-40 w-full border-b border-b-slate-200 dark:border-b-slate-700">
+	<header className="sticky top-0 z-40 w-full border-b border-b-slate-200 dark:border-b-slate-700 bg-background">
 		<div className="container flex h-16 items-center">
 			<Link href={route("homepage")} className="text-2xl font-bold mr-2">
 				<ApplicationLogo iconClassName="w-8 h-8" />
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,26 +1,42 @@
 import { AuthenticatedLayout } from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
+import { H1 } from "@/Components/ui/Typography";
+import mapboxgl, { Map } from "mapbox-gl";
+import { useEffect, useRef, useState } from "react";
 
 export default function Dashboard({ auth }: PageProps) {
+	const mapContainer = useRef<HTMLDivElement | null>(null);
+	const map = useRef<Map | null>(null);
+	const [lng, setLng] = useState(-70.9);
+	const [lat, setLat] = useState(42.35);
+	const [zoom, setZoom] = useState(9);
+
+	useEffect(() => {
+		if (map.current) return;
+		if (mapContainer.current) {
+			map.current = new mapboxgl.Map({
+				accessToken: import.meta.env.VITE_MAPBOX_API_TOKEN,
+				container: mapContainer.current,
+				style: "mapbox://styles/mapbox/streets-v12",
+				center: [lng, lat],
+				zoom: zoom,
+			});
+		}
+	}, []);
+
 	return (
-		<AuthenticatedLayout
-			user={auth.user}
-			header={
-				<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-					Dashboard
-				</h2>
-			}
-		>
+		<AuthenticatedLayout>
 			<Head title="Dashboard" />
 
-			<div className="py-12">
-				<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-					<div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-						<div className="p-6 text-gray-900 dark:text-gray-100">
-							You're logged in!
-						</div>
-					</div>
+			<div className="container py-10">
+				<H1 className="mb-10">Dashboard</H1>
+				<div className="grid sm:grid-cols-2 gap-6">
+					<div
+						ref={mapContainer}
+						className="h-[300px] sm:h-[600px] rounded-xl"
+					/>
+					<div>Hello</div>
 				</div>
 			</div>
 		</AuthenticatedLayout>
