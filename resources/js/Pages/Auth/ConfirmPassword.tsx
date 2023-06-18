7ab22c87b52862414ab65275a5cd71effa5643bd
domnantas <hello@domnantas.lt>
2023-06-18 13:47:49 +0300

    Update confirm password page design

--- resources/js/Pages/Auth/ConfirmPassword.tsx
+++ resources/js/Pages/Auth/ConfirmPassword.tsx
@@ -1,10 +1,18 @@
 import { useEffect, FormEventHandler } from "react";
 import { EmptyLayout } from "@/Layouts/EmptyLayout";
 import InputError from "@/Components/InputError";
-import InputLabel from "@/Components/InputLabel";
-import PrimaryButton from "@/Components/PrimaryButton";
-import TextInput from "@/Components/TextInput";
 import { Head, useForm } from "@inertiajs/react";
+import { Button } from "@/Components/ui/Button";
+import {
+	Card,
+	CardHeader,
+	CardTitle,
+	CardDescription,
+	CardContent,
+	CardFooter,
+} from "@/Components/ui/Card";
+import { Input } from "@/Components/ui/Input";
+import { Label } from "@radix-ui/react-label";
 
 export default function ConfirmPassword() {
 	const { data, setData, post, processing, errors, reset } = useForm({
@@ -27,34 +35,48 @@ export default function ConfirmPassword() {
 		<EmptyLayout>
 			<Head title="Confirm Password" />
 
-			<div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
-				This is a secure area of the application. Please confirm your
-				password before continuing.
+			<div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+				<Card className="w-full max-w-md">
+					<CardHeader>
+						<CardTitle>Confirm Password</CardTitle>
+						<CardDescription>
+							This is a secure area of the application. Please
+							confirm your password before continuing.
+						</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<form id="confirm-password" onSubmit={submit}>
+							<div className="grid w-full items-center gap-4">
+								<div className="grid gap-2">
+									<Label htmlFor="password">Password</Label>
+									<Input
+										id="password"
+										value={data.password}
+										type="password"
+										autoFocus
+										onChange={(event) =>
+											setData(
+												"password",
+												event.target.value
+											)
+										}
+									/>
+									<InputError message={errors.password} />
+								</div>
+							</div>
+						</form>
+					</CardContent>
+					<CardFooter className="flex justify-end gap-2">
+						<Button
+							type="submit"
+							form="confirm-password"
+							disabled={processing}
+						>
+							Confirm
+						</Button>
+					</CardFooter>
+				</Card>
 			</div>
-
-			<form onSubmit={submit}>
-				<div className="mt-4">
-					<InputLabel htmlFor="password" value="Password" />
-
-					<TextInput
-						id="password"
-						type="password"
-						name="password"
-						value={data.password}
-						className="mt-1 block w-full"
-						isFocused={true}
-						onChange={(e) => setData("password", e.target.value)}
-					/>
-
-					<InputError message={errors.password} className="mt-2" />
-				</div>
-
-				<div className="flex items-center justify-end mt-4">
-					<PrimaryButton className="ml-4" disabled={processing}>
-						Confirm
-					</PrimaryButton>
-				</div>
-			</form>
 		</EmptyLayout>
 	);
 }
