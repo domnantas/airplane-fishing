33929a20440e6ac87ea1905cbad951ae759cc964
domnantas <hello@domnantas.lt>
2023-06-18 12:52:04 +0300

    Replace emoji with icons

---
+++ resources/js/Components/ui/Icons.tsx
@@ -0,0 +1,7 @@
+import { LucideIcon, Plane, Fish } from "lucide-react";
+
+export type Icon = LucideIcon;
+export const Icons = {
+	Plane,
+	Fish,
+};
--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,7 +1,8 @@
-import { Link, Head } from "@inertiajs/react";
+import { Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
 import { PrimaryLayout } from "@/Layouts/PrimaryLayout";
-import { H1, H2, H3 } from "@/Components/ui/Typography";
+import { H1, H3 } from "@/Components/ui/Typography";
+import { Icons } from "@/Components/ui/Icons";
 
 export default function Welcome({ auth }: PageProps) {
 	return (
@@ -9,12 +10,13 @@ export default function Welcome({ auth }: PageProps) {
 			<Head title="Airplane Fishing" />
 			<PrimaryLayout isAuthenticated={!!auth.user}>
 				<section className="container mx-auto py-40 text-center text-slate-700 dark:text-slate-300">
-					<H1 className="text-5xl lg:text-7xl">
-						✈ Airplane{" "}
+					<H1 className="text-5xl lg:text-7xl ">
+						<Icons.Plane className="inline align-top w-12 h-12 lg:w-20 lg:h-20" />{" "}
+						Airplane{" "}
 						<span className="text-black dark:text-white">
-							fishing
-						</span>{" "}
-						🎣
+							fishing{" "}
+							<Icons.Fish className="inline align-top w-12 h-12 lg:w-20 lg:h-20" />
+						</span>
 					</H1>
 					<H3 className="mt-8">
 						Track aircraft flying over your head
