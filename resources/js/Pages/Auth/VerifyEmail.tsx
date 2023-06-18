9a7924de92ddf084d511e6361411a801afd21301
domnantas <hello@domnantas.lt>
2023-06-18 13:56:52 +0300

    Update verify email page design

--- resources/js/Pages/Auth/VerifyEmail.tsx
+++ resources/js/Pages/Auth/VerifyEmail.tsx
@@ -2,6 +2,14 @@ import { EmptyLayout } from "@/Layouts/EmptyLayout";
 import PrimaryButton from "@/Components/PrimaryButton";
 import { Head, Link, useForm } from "@inertiajs/react";
 import { FormEventHandler } from "react";
+import { Button } from "@/Components/ui/Button";
+import {
+	Card,
+	CardHeader,
+	CardTitle,
+	CardContent,
+	CardFooter,
+} from "@/Components/ui/Card";
 
 export default function VerifyEmail({ status }: { status?: string }) {
 	const { post, processing } = useForm({});
@@ -16,36 +24,31 @@ export default function VerifyEmail({ status }: { status?: string }) {
 		<EmptyLayout>
 			<Head title="Email Verification" />
 
-			<div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
-				Thanks for signing up! Before getting started, could you verify
-				your email address by clicking on the link we just emailed to
-				you? If you didn't receive the email, we will gladly send you
-				another.
+			<div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+				<Card className="w-full max-w-md">
+					<CardHeader>
+						<CardTitle>Email Verification</CardTitle>
+					</CardHeader>
+					<CardContent>
+						Thanks for signing up! Before getting started, could you
+						verify your email address by clicking on the link we
+						just emailed to you? If you didn't receive the email, we
+						will gladly send you another.
+					</CardContent>
+					<CardFooter className="flex justify-end gap-2">
+						<Button
+							variant="link"
+							className="text-muted-foreground"
+							asChild
+						>
+							<Link href={route("logout")}>Log out</Link>
+						</Button>
+						<Button onClick={submit} disabled={processing}>
+							Resend Verification Email
+						</Button>
+					</CardFooter>
+				</Card>
 			</div>
-
-			{status === "verification-link-sent" && (
-				<div className="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-					A new verification link has been sent to the email address
-					you provided during registration.
-				</div>
-			)}
-
-			<form onSubmit={submit}>
-				<div className="mt-4 flex items-center justify-between">
-					<PrimaryButton disabled={processing}>
-						Resend Verification Email
-					</PrimaryButton>
-
-					<Link
-						href={route("logout")}
-						method="post"
-						as="button"
-						className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-					>
-						Log Out
-					</Link>
-				</div>
-			</form>
 		</EmptyLayout>
 	);
 }
